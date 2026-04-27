85f01a7c5cb0ccc2cac79a406b559b5b5b097118
Pawan-Koder <1213056@genstudents.org>
2026-04-27 10:52:01 +0100

    Feature: Build core app loop, app exit and navigation menu display with initial navigation input.

--- app.py
+++ app.py
@@ -10,21 +10,26 @@ disabled_users = create_disabled_user_list()
 
 # =========================================================
 
+app_is_running = True
+while app_is_running == True:
+
+    # NAVIGATION MENU
+    navigation_menu()
+    navigation_input = int(input('Please enter an index:  '))
 
 
-# NAVIGATION MENU
+    if navigation_input == 1:
+        pass
 
-# CLIENT INPUT
+    # ELIF 2: LIST ACCOUNTS
+        # USER INPUT: VIEW ACTIVE OR DISABLED ACCOUNTS
 
-# ELIF 1: ADD USER
+    # ELIF 3: ENABLE/DISABLE USER
+        # USER INPUT: ENABLE OR DISABLE
+        # VIEW ACCOUNTS
+        # USER INPUTS: SELECT ACCOUNT
 
-# ELIF 2: LIST ACCOUNTS
-    # USER INPUT: VIEW ACTIVE OR DISABLED ACCOUNTS
-
-# ELIF 3: ENABLE/DISABLE USER
-    # USER INPUT: ENABLE OR DISABLE
-    # VIEW ACCOUNTS
-    # USER INPUTS: SELECT ACCOUNT
-
-# ELIF 4: EXIT
+    if navigation_input == 0:
+        app_is_running = 'False'
+    
 

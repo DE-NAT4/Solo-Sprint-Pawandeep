59db960f144c845d961b2c9613981b151b23c6eb
Pawan-Koder <1213056@genstudents.org>
2026-04-27 10:43:54 +0100

    Planning outline.

--- app.py
+++ app.py
@@ -1,6 +1,30 @@
 from users import *
 
+# =========================================================
+# INITIALISATION 
+
+
 active_users = create_active_user_list()
 disabled_users = create_disabled_user_list()
-print_accounts(active_users, 'Active')
-print_accounts(disabled_users, 'Disabled')
+
+
+# =========================================================
+
+
+
+# NAVIGATION MENU
+
+# CLIENT INPUT
+
+# ELIF 1: ADD USER
+
+# ELIF 2: LIST ACCOUNTS
+    # USER INPUT: VIEW ACTIVE OR DISABLED ACCOUNTS
+
+# ELIF 3: ENABLE/DISABLE USER
+    # USER INPUT: ENABLE OR DISABLE
+    # VIEW ACCOUNTS
+    # USER INPUTS: SELECT ACCOUNT
+
+# ELIF 4: EXIT
+
